bb8c3f10b9654d5db8e138ed2096ba5657a91856
Elliot Meldrum <meldrum2718@gmail.com>
2024-10-25 16:08:56 -0700

    use einsum

--- code/rtn.py
+++ code/rtn.py
@@ -1,92 +1,171 @@
 import numpy as np
 
-def inv_p(perm):
-    """ from https://stackoverflow.com/questions/9185768/inverting-permutations-in-python """
-    inv = np.empty_like(perm)
-    inv[perm,] = np.arange(len(inv), dtype=inv.dtype)
-    return inv   
+
+def ceinsum(X, Y, C, color, verbose=False) -> np.ndarray:
+    """ Return einsum of X and some subtensor of Y, with all indices
+        'read' from C.
+
+        X, ker are ndarrays with n = X.shape[i] = X.shape[j] = ker.shape[k] for all valid i,j,k.
+        C is a 2d array.
+
+    """
+    def p(*args):
+        if verbose:
+            print(*args)
+
+    n = X.shape[0]
+
+    if color: # design decision. just averaging color channels of C.
+        C = np.mean(C, axis=-1)
+
+    xnd = len(X.shape)
+    ynd = len(Y.shape)
+
+    p('x.s', X.shape, 'xnd', xnd)
+    p('y.s', Y.shape, 'ynd', ynd)
+    p('c.s', C.shape)
+
+    ch = C.shape[0]
+    n_samples = 5
+    sh = ch // n_samples # sampling height
+
+    p('sh', sh)
+
+    px = np.argsort(np.sum(C[0*sh:1*sh, 0:xnd], axis=0)) # X perm
+    po = np.argsort(np.sum(C[1*sh:2*sh, 0:xnd], axis=0)) # out perm
+    py = np.argsort(np.sum(C[2*sh:3*sh, 0:ynd], axis=0)) # Y perm
+
+    p('px.s', px.shape)
+    p('po.s', po.shape)
+    p('py.s', py.shape)
+
+    knd = np.argmax(np.sum(C[2*sh:3*sh, 0:ynd-2])) + 2 # number of kernel dimensions
+    nki = ynd - knd # number of kernel indices
+
+    pk = np.argsort(np.sum(C[3*sh:4*sh, 0:knd], axis=0)) # kernel perm
+
+    ki = tuple(np.argmax(C[4*sh:4*sh+n, 0:nki], axis=0)) # kernel indices
+    
+    p('knd', knd)
+    p('nki', nki)
+    p('ki', ki)
+    p('pk.s', pk)
+
+    ker = Y.transpose(*py)
+
+    p('ker1.s', ker.shape)
+    ker = ker[ki]
+    p('ker.s', ker.shape)
+    p()
+
+    out = np.einsum(X, px, ker, pk, po)
+    return out
 
 
 class RTN:
-    """ Recurrent tensor network.
-        X is an (n^k, n^k) array.
-        We also think of X as having shape:
-            (n,  n)^(k-1) (n x n)^(k-1)
-            (n, n, n^2(k-1) )
-            (n, n, (n, n, n^(2k-2) ) )
+    def __init__(self, n, k, batch_dim=0, color=False, clip_min=-2, clip_max=2):
+        """ initialize a random (n**k, n**k) state matrix.
 
-        and as an (n**2, n**2)**(k-1).
-    """
-
-    def __init__(self, n, k, color=False, clip_min=-2, clip_max=2):
-        """ initialize a random (n**k, n**k) state matrix."""
+        TODO implement batch dim properly. right now going to implement
+             something not respecting independence along batch time. will
+             rectify soon
+        """
         self.n = n
         self.k = k
+        self.batch_dim = batch_dim
+        self.color = color
         self.clip_min = clip_min
         self.clip_max = clip_max
-        self.color = color
-        if self.color: # add a color dim
-            self.X = np.random.randn(n**k, n**k, n)
-        else:
-            self.X = np.random.randn(n**k, n**k)
+        self.ndim = 2 * k
 
-    def input(self, inp):
-        """ inject inp into bottom right corner of self.X.
-            TODO do this better, this feels crude.
-        """
-        if inp is None:
-            return
-        h, w = inp.shape[:2]
-        self.X[-h:, -w:] = inp
+        self.flat_shape = (n**k, n**k)
 
-    def output(self, h, w):
+        if batch_dim > 0:
+            self.flat_shape = (batch_dim,) + self.flat_shape
+        if self.color:
+            assert n >= 3, 'need n >=3 for color'
+            self.flat_shape = self.flat_shape + (n,)
+            self.ndim += 1
+
+        self.nested_shape = tuple([n for _ in range(self.ndim)])
+        self.X: np.ndarray = np.random.randn(*self.flat_shape).reshape(self.nested_shape)
+
+    def output(self):
         """ project state out to lower dimensional space.
         for now just done by returning bottom right (h, w) submatrix.
 
-        TODO think about introducing conv stems for outpus. would encourage
-        interpratible, spatially localized representations I think..
+        ## TODO think about introducing conv stems for outpus. would encourage
+        ## interpratible, spatially localized representations I think..
+            think this should be done outside the rtn..
 
-        But for now, we just pull output from some submatrix of the state, and
-        let the rtn implicitly find good representations. if we have a regular
-        sampling period, this should work out nicely.
-        """
-        return self.X[-h:, -w:]
-
-    def prod(self, ker, latent_shape, latent_perm) -> np.ndarray:
-        """ reshape x0 into latent_shape, apply permutation, multiply by ker,
-            then invert permutation and reshape back to original shape.
-        """
-        inv_perm = inv_p(latent_perm)
-        x1 = self.X.reshape(*latent_shape).transpose(*latent_perm)
-        out1 = x1 @ ker
-        out0 = out1.transpose(*inv_perm).reshape(self.X.shape)
-        return out0
-
-    def step(self, latent_perm, ker, inp=None, alpha=0.01, sigma=0) -> np.ndarray:
-        """
-        Let dXdt = X @ ker, then update: X += alpha * dXdt
-
-        sigma is a 'noise parameter', paremeterizing the stddev of the base
-        noise, which is scaled by prediction error and added to the model.
+        ## But for now, we just pull output from some submatrix of the state, and
+        ## let the rtn implicitly find good representations. if we have a regular
+        ## sampling period, this should work out nicely.
 
         """
-        if inp is not None:
-            self.input(inp)
-
         if self.color:
-            latent_shape = (self.n for _ in range(2*self.k + 1))
-        else:
-            latent_shape = (self.n for _ in range(2*self.k))
-        dX = self.prod(ker, latent_shape=latent_shape, latent_perm=latent_perm)
-        self.X = self.X + alpha * dX
-        # self.input(inp)
+            return self.X.reshape(self.flat_shape)[:, :, :3] # just use first three color channels for display
+        return self.X.reshape(self.flat_shape)
 
-        if (inp is not None) and (sigma > 0):
-            noise = np.random.normal(scale=sigma, size=self.X.shape)
-            err = np.linalg.norm(inp - self.output(*inp.shape[:2]))
+
+    def step(self, ker, C, inp=None, alpha=0.01, noise_fbk=0) -> np.ndarray:
+        """
+        update: X += alpha * dXdt
+
+        noise_fbk controls the size of the base noise, which is then scaled by
+        prediction error and added to the state.
+
+        """
+        dX = ceinsum(self.X, self.X, self.X.reshape(self.flat_shape), color=self.color)
+        self.X = self.X + alpha * dX
+
+        if noise_fbk > 0 and inp is not None:
+            noise = np.random.normal(scale=noise_fbk, size=self.X.shape)
+            err = np.linalg.norm(inp - self.output())
             noise = noise * err
             self.X = self.X + noise
 
         self.X = np.clip(self.X, self.clip_min, self.clip_max)
 
-        return self.X
+        return self.output()
+
+
+
+
+    ## I dont think i want this..
+    # def input(self, inp):
+    #     """ inject inp into bottom right corner of self.X.
+    #         TODO do this better, this feels crude.
+    #     """
+    #     if inp is None:
+    #         print('trying to input None into rtn .. why tho?')
+    #         return
+    #     h, w = inp.shape[:2]
+    #     x = self.X
+    #     if self.batch_dim:
+    #         x = x.reshape(self.n**self.batch_dim, self.n**k, self.n**self.batch_dim, self.n**k).transpose(0, 2, 1, 1)
+
+    #     if self.color:
+    #         x[-h:, -w:, :3] += inp
+    #     else:
+    #         x[-h:, -w:] += inp
+
+    #     if self.batch_dim:
+    #         # reshape back.
+    #         x = x.reshape(self.n**self.batch_dim, self.n**k, self.n**self.batch_dim, self.n**k).transpose(0, 2, 1, 1)
+    #         x = x.transpose(0, 2, 1, 3).reshape(self.X.shape)
+    #         print('unbatching')
+
+    #     self.X = x
+
+
+    # def prod(self, ker, latent_shape, latent_perm) -> np.ndarray:
+    #     """ reshape x0 into latent_shape, apply permutation, multiply by ker,
+    #         then invert permutation and reshape back to original shape.
+    #     """
+    #     inv_perm = inv_p(latent_perm)
+    #     x1 = self.X.reshape(*latent_shape).transpose(*latent_perm)
+    #     out1 = x1 @ ker
+    #     out0 = out1.transpose(*inv_perm).reshape(self.X.shape)
+    #     return out0
+
--- code/run_rtn.py
+++ code/run_rtn.py
@@ -1,56 +1,14 @@
 import argparse
+# import tracemalloc
 
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Slider
 
 from rtn import RTN
 from utils import get_video_capture, rgb2grey, normalize
-
-
-def sample_prod_params(X, n, k, color=False):
-    """ Return parameters for a product of X with a subtensor of itself.
-
-        Just going to do a really simple sampling for now. future, perhaps
-        make it be more built/structured, like with a conv stem or so. For now
-        it is going to just be directly computed from reading permutations and
-        indices from submatrices of X in the top left-ish vicinity of X.
-
-        Returns:
-            (pre_xlp, pre_ksp, pre_ki, pre_klp), X_latent_perm, ker
-
-        TODO think about instead returning ksp, knd, ki, klp. q: is this a nice general representation of the parameters into the product?. a: it seems nice for products between X and a subtensor of X.
-
-    """
-    if color:
-        X_ndim = 2 * k + 1
-    else:
-        X_ndim = 2 * k
-
-    nested_shape = (n for _ in range(X_ndim))
-    
-    if color:
-        X_color = X
-        X = rgb2grey(X)
-    X_latent_perm =   np.argsort(pre_xlp := np.sum(X[0:n, 0:X_ndim], axis=0))
-    ker_sample_perm = np.argsort(pre_ksp := np.sum(X[2*n:3*n, 0:X_ndim], axis=0))
-    ker_ndim =         np.argmax(pre_knd := np.sum(X[n:2*n, 0:k-2], axis=0)) + 2 # ker_ndim in [2, 3, ..., k]
-    ker_indices = tuple(np.argmax(pre_ki := X[3*n:4*n, 0:X_ndim-ker_ndim], axis=0))
-    ker_latent_perm = np.argsort(pre_klp := np.sum(X[4*n:5*n, 0:ker_ndim], axis=0))
-
-
-    # print('x.s', X.shape, 'x.numel', X.size)
-    # print('nested shape', nested_shape, 'ns.size')
-    if color:
-        X = X_color
-
-    ker = X.reshape(*nested_shape)
-    ker = ker.transpose(*ker_sample_perm)
-    ker = ker[ker_indices]
-    ker = ker.transpose(*ker_latent_perm)
-
-    return (pre_xlp, pre_ksp, pre_ki, pre_klp), X_latent_perm, ker
 
 
 def main(args):
@@ -61,17 +19,21 @@ def main(args):
         if args.video_input:
             cap = get_video_capture()
 
-        if None in (args.h, args.w):
-            args.h = args.w = n**(k-1)
+        h = w = n**k
 
-        rtn = RTN(n, k, color=args.color, clip_min=args.clip_min, clip_max=args.clip_max)
+        rtn = RTN(n, k, batch_dim=args.batch_dim, color=args.color, clip_min=args.clip_min, clip_max=args.clip_max)
 
         fig = plt.figure(figsize=(14, 7))
         im_ax = fig.add_subplot()
 
-        im = im_ax.imshow(normalize(rtn.X), cmap='grey')
+        im = im_ax.imshow(normalize(rtn.output()), cmap='grey')
+
+        alpha = 0
+        noise_fbk = 0
 
         def frame_gen():
+            nonlocal alpha
+            nonlocal noise_fbk
             t = 0
             while True:
                 t += 1
@@ -80,30 +42,46 @@ def main(args):
                     _, inp = cap.read()
                     # convert inp to rgb with shape (h, w) and pixel values in [0, 1]
                     inp = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
-                    inp = cv2.resize(inp, (args.w, args.h), interpolation = cv2.INTER_AREA) / 255.0
+                    inp = cv2.resize(inp, (w, h), interpolation = cv2.INTER_AREA) / 255.0
                     if not args.color:
                         inp = rgb2grey(inp)
 
-                pre, xlp, ker = sample_prod_params(rtn.X, rtn.n, rtn.k, args.color)
-                state = rtn.step(latent_perm=xlp, ker=ker, inp=inp, alpha=args.alpha, sigma=args.sigma).copy()
+                ker = rtn.X if inp is None else inp
+                state = rtn.step(ker=ker, C=rtn.X.reshape(rtn.flat_shape), inp=inp, alpha=alpha, noise_fbk=noise_fbk)
+
                 yield t, state
 
         def draw_func(frame):
             t, state = frame
             im.set_data(normalize(state))
+            # im.axes.figure.canvas.draw_idle()
             fig.suptitle(str(t))
-            return im
+            return [im]
 
         ani = FuncAnimation(
             fig,
             func=draw_func,
             frames=frame_gen,
-            interval=50,
-            save_count=0,
+            interval=10,
+            save_count=1,
         )
+
+        def update_alpha(x):
+            nonlocal alpha
+            alpha = x
+        alpha_slider = Slider(fig.add_axes([0.2, 0.05, 0.65, 0.03]), label=r'$\alpha$', valmin=args.alphamin, valmax=args.alphamax, valinit=alpha)
+        alpha_slider.on_changed(update_alpha)
+
+        def update_noise_fbk(x):
+            nonlocal noise_fbk
+            noise_fbk = x
+        noise_fbk_slider = Slider(fig.add_axes([0.2, 0.10, 0.65, 0.03]), label='Noise feedback', valmin=args.noise_fbk_min, valmax=args.noise_fbk_max, valinit=noise_fbk)
+        noise_fbk_slider.on_changed(update_noise_fbk)
 
         plt.show()
 
+    except Exception as e:
+        print(e)
     finally:
         if cap is not None:
             cap.release()
@@ -116,26 +94,98 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--n', required=True, type=int)
     parser.add_argument('--k', required=True, type=int)
-    parser.add_argument('--alpha', required=False, default='0.01', type=float)
     parser.add_argument('--video_input', action='store_true')
     parser.add_argument('--color', '-c', action='store_true')
-    parser.add_argument('--h', default=None, type=int)
-    parser.add_argument('--w', default=None, type=int)
     parser.add_argument('--clip_min', '-cmi', default=-2, type=float)
     parser.add_argument('--clip_max', '-cma', default=2, type=float)
     parser.add_argument('--sample_period', '-sp', default=None, type=int)
-    parser.add_argument('--sigma', default=0, type=float)
+    parser.add_argument('--alphamin', required=False, default='0', type=float)
+    parser.add_argument('--alphamax', required=False, default='1', type=float)
+    parser.add_argument('--noise_fbk_min', required=False, default='0', type=float)
+    parser.add_argument('--noise_fbk_max', required=False, default='0.01', type=float)
+    parser.add_argument('--batch_dim', '-bd', default=0, type=int)
+
 
 
     args = parser.parse_args()
 
     # validate args
-    if any((args.sample_period, args.color, args.h, args.w)):
+    if args.sample_period:
         args.video_input = True
     if args.video_input:
         args.sample_period = args.sample_period or 1
 
     if args.color:
-        assert args.n == 3, 'handling color for n != 3 not supported.'
+        assert args.n >= 3, 'handling color for n < 3 not supported.'
 
     main(args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### deprecated ###########
+
+# def sample_prod_params(X, n, k, batch_dim=None, color=False):
+#     """ Return parameters for a product of X with a subtensor of itself.
+# 
+#         Just going to do a really simple sampling for now. future, perhaps
+#         make it be more built/structured, like with a conv stem or so. For now
+#         it is going to just be directly computed from reading permutations and
+#         indices from submatrices of X in the top left-ish vicinity of X.
+# 
+#         Returns:
+#             (pre_xlp, pre_ksp, pre_ki, pre_klp), X_latent_perm, ker
+# 
+#         TODO think about instead returning ksp, knd, ki, klp. q: is this a nice general representation of the parameters into the product?. a: it seems nice for products between X and a subtensor of X.
+# 
+#     """
+#     
+#     if batch_dim is not None:
+#         X = X.reshape(n**batch_dim, n**k, n**batch_dim, n**k).reshape(0, 2, 1, 3)
+# 
+#     if color:
+#         X_ndim = 2 * k + 1
+#     else:
+#         X_ndim = 2 * k
+# 
+#     nested_shape = (n for _ in range(X_ndim))
+#     
+#     if color:
+#         X_color = X
+#         X = rgb2grey(X)
+#     X_latent_perm =   np.argsort(pre_xlp := np.sum(X[0:n, 0:X_ndim], axis=0))
+#     ker_sample_perm = np.argsort(pre_ksp := np.sum(X[2*n:3*n, 0:X_ndim], axis=0))
+#     ker_ndim =         np.argmax(pre_knd := np.sum(X[n:2*n, 0:k-2], axis=0)) + 2 # ker_ndim in [2, 3, ..., k]
+#     ker_indices = tuple(np.argmax(pre_ki := X[3*n:4*n, 0:X_ndim-ker_ndim], axis=0))
+#     ker_latent_perm = np.argsort(pre_klp := np.sum(X[4*n:5*n, 0:ker_ndim], axis=0))
+# 
+# 
+#     # print('x.s', X.shape, 'x.numel', X.size)
+#     # print('nested shape', nested_shape, 'ns.size')
+#     if color:
+#         X = X_color
+# 
+#     ker = X.reshape(*nested_shape)
+#     ker = ker.transpose(*ker_sample_perm)
+#     ker = ker[ker_indices]
+#     ker = ker.transpose(*ker_latent_perm)
+# 
+#     return (pre_xlp, pre_ksp, pre_ki, pre_klp), X_latent_perm, ker
